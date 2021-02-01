bc891a11db23c030708af05c9526f117aca04bb0
LuckyPoire <alexandrebrunet85@gmail.com>
2021-02-01 16:29:51 +0100

    firebase and firestore initialization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,27 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire'
+import { AngularFirestoreModule } from '@angular/fire/firestore'
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyA5M3SiENGyjS8ryv1VBIudX74jbLyyNsw",
+  authDomain: "localfeeds-5efd7.firebaseapp.com",
+  projectId: "localfeeds-5efd7",
+  storageBucket: "localfeeds-5efd7.appspot.com",
+  messagingSenderId: "1081422647180",
+  appId: "1:1081422647180:web:41753de9735a634f8950a9"
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig)
+  ],
   providers: [
     StatusBar,
     SplashScreen,
