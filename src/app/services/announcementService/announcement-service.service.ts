be0a6c2c276190c20e5869ff1f7380dc34000460
Alexandre Brunet <alexandre.brunet@etudiant.univ-lr.fr>
2021-02-13 19:49:11 +0100

    implement function in announcementsService

--- src/app/services/announcementService/announcement-service.service.ts
+++ src/app/services/announcementService/announcement-service.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore'
+import { Observable } from 'rxjs'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnnouncementServiceService {
 
-  constructor() { }
+  constructor(
+    private firestore: AngularFirestore
+  ) { }
+
+  public getAnnouncements(): Observable<Object> {
+    return this.firestore.collection("announcements").valueChanges();
+  }
+
+  public getAnnouncement(id: string): Observable<Object> {
+    return this.firestore.collection("announcements").doc(id).valueChanges();
+  }
+
 }
