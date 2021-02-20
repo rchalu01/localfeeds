698dab1246fa6f34ff37d14f4d3cc2da4a892527
Naimzz <lucienbarcon@gmail.com>
2021-02-20 11:31:12 +0100

    Display productTypes in product-cell

--- src/app/components/productor-list-cell/productor-list-cell.component.ts
+++ src/app/components/productor-list-cell/productor-list-cell.component.ts
@@ -8,11 +8,25 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ProductorListCellComponent implements OnInit {
 
   @Input() productor;
+
   favoriteImage: string = "loveWhite";
+
+  meatImage: string;
+  eggImage: string;
+  vegetableImage: string;
+  fruitImage: string;
+  milkImage: string;
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+
+    this.productor.productType.includes(1) ? this.meatImage = "meatColor": this.meatImage = "meat";
+    this.productor.productType.includes(2) ? this.eggImage = "eggsColor": this.eggImage = "eggs";
+    this.productor.productType.includes(3) ? this.vegetableImage = "vegetableColor": this.vegetableImage = "vegetable";
+    this.productor.productType.includes(4) ? this.fruitImage = "fruitsColor": this.fruitImage = "fruits";
+    this.productor.productType.includes(5) ? this.milkImage = "milkColor": this.milkImage = "milk";
+  }
 
   public addToFavorite() {
 
