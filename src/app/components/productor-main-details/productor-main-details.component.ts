1c8aef178d1dfbcbbbfd4d824b954594f540de76
Naimzz <lucienbarcon@gmail.com>
2021-02-24 14:21:15 +0100

    Style of productor main details

--- src/app/components/productor-main-details/productor-main-details.component.ts
+++ src/app/components/productor-main-details/productor-main-details.component.ts
@@ -11,6 +11,12 @@ export class ProductorMainDetailsComponent implements OnInit {
   @Input() productorId;
   productor: any;
 
+  meatImage: string;
+  eggImage: string;
+  vegetableImage: string;
+  fruitImage: string;
+  milkImage: string;
+
   constructor(
     private productorService: ProductorServiceService
   ) { }
@@ -18,6 +24,11 @@ export class ProductorMainDetailsComponent implements OnInit {
   ngOnInit() {
     this.productorService.getProductor(this.productorId).subscribe((value) => {
       this.productor = value;      
+      this.productor.productType.includes(1) ? this.meatImage = "meatColor": this.meatImage = "meat";
+      this.productor.productType.includes(2) ? this.eggImage = "eggsColor": this.eggImage = "eggs";
+      this.productor.productType.includes(3) ? this.vegetableImage = "vegetableColor": this.vegetableImage = "vegetable";
+      this.productor.productType.includes(4) ? this.fruitImage = "fruitsColor": this.fruitImage = "fruits";
+      this.productor.productType.includes(5) ? this.milkImage = "milkColor": this.milkImage = "milk";
     });
   }
 
